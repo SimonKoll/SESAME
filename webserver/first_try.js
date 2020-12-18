d3520d89a16eae82d8647e46f638e48849f2ec79
SimonKoll <simonkoll20@gmail.com>
2020-12-18 09:46:39 +0100

    First implementations of Webserver

--- webserver/first_try.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-http.createServer(function(req, res) {
-    // add a HTTP header:
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('Hello World!');
-    res.end();
-}).listen(8080);
