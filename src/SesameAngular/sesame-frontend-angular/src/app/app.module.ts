69b89fc574bc0e15b0dab2de13a1290089ca86cf
Benjamin Golic <golicbenjamin@gmx.at>
2021-02-19 09:21:01 +0100

    Merge conflict handeld

--- src/SesameAngular/sesame-frontend-angular/src/app/app.module.ts
+++ src/SesameAngular/sesame-frontend-angular/src/app/app.module.ts
@@ -5,22 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
-<<<<<<< HEAD
 import { RegisterComponent } from './register/register.component';
-=======
 import { SettingsComponent } from './settings/settings.component';
->>>>>>> 776bca8d97a8c63f8eebce04be11f7e3b9b00e96
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     MenuComponent,
-<<<<<<< HEAD
+    SettingsComponent,
     RegisterComponent
-=======
-    SettingsComponent
->>>>>>> 776bca8d97a8c63f8eebce04be11f7e3b9b00e96
   ],
   imports: [
     BrowserModule,
