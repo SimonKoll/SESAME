f776d997bb8be8dad323a6eaffc9406cf976a056
SimonKoll <simonkoll20@gmail.com>
2021-02-19 09:22:12 +0100

    test commiting nr2

--- src/SesameAngular/sesame-frontend-angular/src/app/app.module.ts
+++ src/SesameAngular/sesame-frontend-angular/src/app/app.module.ts
@@ -5,22 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
-<<<<<<< HEAD
-import { RegisterComponent } from './register/register.component';
-=======
+import { RegisterComponent } from './register/register.component';  
 import { SettingsComponent } from './settings/settings.component';
->>>>>>> 776bca8d97a8c63f8eebce04be11f7e3b9b00e96
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     MenuComponent,
-<<<<<<< HEAD
-    RegisterComponent
-=======
+    RegisterComponent,
     SettingsComponent
->>>>>>> 776bca8d97a8c63f8eebce04be11f7e3b9b00e96
   ],
   imports: [
     BrowserModule,
@@ -29,4 +23,4 @@ import { SettingsComponent } from './settings/settings.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
