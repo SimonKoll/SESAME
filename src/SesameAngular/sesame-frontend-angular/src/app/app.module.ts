776bca8d97a8c63f8eebce04be11f7e3b9b00e96
Benjamin Golic <golicbenjamin@gmx.at>
2021-02-19 09:13:06 +0100

    Component Settings added

--- src/SesameAngular/sesame-frontend-angular/src/app/app.module.ts
+++ src/SesameAngular/sesame-frontend-angular/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    MenuComponent
+    MenuComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
