1c48ed8c27331cabab4d3a3678ce030134997f2e
SimonKoll <simonkoll20@gmail.com>
2021-04-27 12:12:53 +0200

    commit from pi before switching OS

--- src/facerecognition/writeJSON.py
+++ src/facerecognition/writeJSON.py
@@ -5,11 +5,11 @@ from datetime import datetime
 
 now = datetime.now()
 
-def write_json(data, filename='../frontend_current/sesame_web/src/assets/entries.json'): 
+def write_json(data, filename='../frontend_current/sesame-web/src/assets/entries.json'): 
 	with open(filename,'w') as f: 
 		json.dump(data, f, indent=4) 
 def writeEntriesToJson(currentname):
-    with open('../frontend_current/sesame_web/src/assets/entries.json') as json_file: 
+    with open('../frontend_current/sesame-web/src/assets/entries.json') as json_file: 
         data = json.load(json_file)
             
         dt = now.strftime("%d/%m/%Y %H:%M:%S")
