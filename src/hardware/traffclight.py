d29ecc7b5465a426ca1a2dd98b43eb2d8bbd0059
SimonKoll <simonkoll20@gmail.com>
2021-11-04 16:33:37 +0100

    renew traffclight.py

--- src/hardware/traffclight.py
+++ src/hardware/traffclight.py
@@ -1,44 +1,16 @@
 import RPi.GPIO as GPIO
 import time
-import signal
-import sys
-
-# Setup
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(22,GPIO.OUT)
-GPIO.setup(18,GPIO.OUT)
-GPIO.setup(16,GPIO.OUT)
-# Turn off all lights when user ends demo
-def allLightsOff(signal, frame):
-	GPIO.output(22, False)
-	GPIO.output(18, False)
-	GPIO.output(16, False)
-	GPIO.cleanup()
-	sys.exit(0)
-
-signal.signal(signal.SIGINT, allLightsOff)
-
-# Loop forever
+def setup():
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(25,GPIO.OUT)
+def loop():
+        GPIO.output(25,True)
+        time.sleep(1)
+        GPIO.output(25,False)
+        time.sleep(1)
+setup()
 while True:
-	# Red
-	GPIO.output(9, True)
-	time.sleep(3)
-
-	# Red and amber
-	GPIO.output(18, True)
-	time.sleep(1)
-	
-	# Green
-	GPIO.output(9, False)
-	GPIO.output(18, False)
-	GPIO.output(16, True)
-	time.sleep(5)
-	
-	# Amber
-	GPIO.output(16, False)
-	GPIO.output(18, True)
-	time.sleep(2)
-	
-	# Amber off (red comes on at top of loop)
-	GPIO.output(18, False)
-
+        loop()
+finally:
+    GPIO.cleanup()
