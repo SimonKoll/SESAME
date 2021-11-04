c9bec67b2823203c7b69f448b0b4a974efcf9f42
SimonKoll <simonkoll20@gmail.com>
2021-11-04 16:58:21 +0100

    we have power!

--- src/hardware/motor.py
+++ src/hardware/motor.py
@@ -9,22 +9,12 @@ p = GPIO.PWM(servoPIN, 50) # GPIO 17 als PWM mit 50Hz
 p.start(2.5) # Initialisierung
 try:
   while True:
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
+    p.ChangeDutyCycle(0.5)
+    time.sleep(10)
     p.ChangeDutyCycle(12.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(2.5)
-    time.sleep(0.5)
+    time.sleep(10)
+    p.ChangeDutyCycle(0.5)
+
 except KeyboardInterrupt:
   p.stop()
   GPIO.cleanup()
