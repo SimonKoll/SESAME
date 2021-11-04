307701425d572bea0036d90369f5bed0ef6fcfe3
SimonKoll <simonkoll20@gmail.com>
2021-11-04 16:49:33 +0100

    first try on motorizing sesame

--- src/hardware/motor.py
+++ src/hardware/motor.py
@@ -1,21 +1,30 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BOARD)
+servoPIN = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
 
-GPIO.setup(13,GPIO.OUT)
-
-p = GPIO.PWM(7,50)
-p.start(7.5)
-
+p = GPIO.PWM(servoPIN, 50) # GPIO 17 als PWM mit 50Hz
+p.start(2.5) # Initialisierung
 try:
-        while True:
-                p.ChangeDutyCycle(7.5)
-                time.sleep(1)
-                p.ChangeDutyCycle(12.5)
-                time.sleep(1)
-                p.ChangeDutyCycle(2.5)
-                time.sleep(1)
-
+  while True:
+    p.ChangeDutyCycle(5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(7.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(10)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(12.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(10)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(7.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(2.5)
+    time.sleep(0.5)
 except KeyboardInterrupt:
-        GPIO.cleanup()
+  p.stop()
+  GPIO.cleanup()
